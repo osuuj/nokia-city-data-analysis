d59d05088e468b51e8badfea9b2643c4ad23e887
osuuj <superjuuso@gmail.com>
2025-02-13 20:05:12 +0200

    Ready for frontend and Fastapi development.

--- server/backend/routers/companies.py
+++ server/backend/routers/companies.py
@@ -1,64 +1,26 @@
-"""This module defines the API endpoints for retrieving company data.
-
-- Provides endpoints for paginated company retrieval.
-- Uses Pydantic schemas for response validation.
-- Depends on database services for data access.
-
-"""
-
-from typing import List
+from typing import Dict, List
 
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 
 from server.backend.database import get_db
-from server.backend.schemas.address_schema import AddressSchema
-from server.backend.schemas.company_schema import CompanySchema
-from server.backend.services.company_service import (
-    get_paginated_addresses,
-    get_paginated_companies,
-)
+from server.backend.services.company_service import get_business_data_by_city
 
 router = APIRouter()
 
 
-@router.get("/companies", response_model=List[CompanySchema])
-def read_companies(
-    page: int = Query(1, alias="page", description="Page number"),
-    page_size: int = Query(
-        10, alias="page_size", description="Number of items per page"
-    ),
+@router.get("/businesses_by_city", response_model=List[Dict[str, str]])
+def read_businesses_by_city(
+    city: str = Query(..., description="City name to filter by"),
     db: Session = Depends(get_db),
-) -> List[CompanySchema]:
-    """Retrieve paginated companies.
+) -> List[Dict[str, str]]:
+    """Retrieve business data by city.
 
     Args:
-        page (int): Page number.
-        page_size (int): Number of items per page.
+        city (str): City name to filter by.
         db (Session): Database session.
 
     Returns:
-        List[CompanySchema]: List of companies.
+        List[Dict[str, str]]: List of business data.
     """
-    return get_paginated_companies(db, page, page_size)
-
-
-@router.get("/addresses", response_model=List[AddressSchema])
-def read_paginated_addresses(
-    page: int = Query(1, alias="page", description="Page number"),
-    page_size: int = Query(
-        10, alias="page_size", description="Number of items per page"
-    ),
-    db: Session = Depends(get_db),
-) -> List[AddressSchema]:
-    """Endpoint to retrieve paginated addresses.
-
-    Args:
-        page (int): Page number.
-        page_size (int): Number of items per page.
-        db (Session): Database session.
-
-    Returns:
-        List[AddressSchema]: List of addresses.
-    """
-    return get_paginated_addresses(db, page, page_size)
+    return get_business_data_by_city(db, city)
