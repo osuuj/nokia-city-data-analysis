38cefc1527903655bc2cca0b10c2931541aad02f
osuuj <superjuuso@gmail.com>
2025-03-12 13:36:33 +0200

    Lots of stuff and now some of them seem to work.

--- client/components/ui/osuuj-icon.tsx
+++ client/components/ui/osuuj-icon.tsx
@@ -2,9 +2,20 @@
 
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function Logo() {
   const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    // Render a placeholder while the theme is being determined
+    return <div style={{ width: '60px', height: '60px' }} />;
+  }
 
   const logoSrc = theme === 'light' ? '/ouuj_black.svg' : '/ouuj_color.svg';
 
