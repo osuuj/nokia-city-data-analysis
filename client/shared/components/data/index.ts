dc22c0917ca80525531b5740b90d1f9154688688
osuuj <superjuuso@gmail.com>
2025-05-03 13:48:36 +0300

    FIX

--- client/shared/components/data/index.ts
+++ client/shared/components/data/index.ts
@@ -1,2 +1,1 @@
 export { DataLoader } from './DataLoader';
-export { default as Preloader } from './Preloader';
--- client/shared/components/error/index.ts
+++ client/shared/components/error/index.ts
@@ -1,4 +1,2 @@
 export { ErrorBoundary } from './ErrorBoundary';
 export { ErrorMessage } from './ErrorMessage';
-export { withErrorBoundary } from './withErrorBoundary';
-export { ErrorFallback } from './ErrorFallback';
