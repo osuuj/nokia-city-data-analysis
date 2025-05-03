6f248e82c143fb232aa125e86f42b8f2578941d8
osuuj <superjuuso@gmail.com>
2025-05-03 14:16:47 +0300

    FIX

--- client/features/dashboard/hooks/data/index.ts
+++
@@ -1,9 +0,0 @@
-export { useDashboardData } from './useDashboardData';
-export { useDashboardLoading } from './useDashboardLoading';
-export { useCities, useCity, useCityStatistics } from './useCities';
-export { useCompanies, useCompany, useCompanyStatistics } from './useCompanies';
-export { useFetchCities, useFetchCompanies } from './useCompaniesQuery';
-export { useFilteredBusinesses } from './useFilteredBusinesses';
-export { useData } from './useData';
-export { useDashboardDataQuery } from './useDashboardDataQuery';
-export { useGeoJSONData, useCitiesData } from './useGeoJSONData';
